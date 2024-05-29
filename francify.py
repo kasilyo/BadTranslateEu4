16dde9395539626f38199fd5d977d11a7a0ddf1e
Kasilyo <kasiyokiwi@gmail.com>
2024-05-29 17:43:14 -0600

    Updating

--- francify.py
+++ francify.py
@@ -1,21 +1,23 @@
 import os
+sp = None
+nl = []
+temp = ""
 
-
-z = os.listdir("localization")
+z = os.listdir(".\\localisation")
 for fp in z:
-    l = open("localisation\\" + fp, "r", encoding="UTF-8")
+    l = open(".\\localisation\\" + fp, "r", encoding="UTF-8")
     lines = l.readlines()
     sp = None
     nl = []
     temp = ""
     nl.append(lines[0])
     for x in lines[1:]:
-        if x.find("#") == -1:
+        if x.find(":") != -1:
             sp = x.split(" \"")
             tmp = sp[0] + " \"France\"\n"
             nl.append(tmp)
     l.close()
-    l = open("localization\\" + fp, "w", encoding="UTF-8")
+    l = open(".\\localisation\\" + fp, "w", encoding="UTF-8")
     for x in nl:
         l.write(x)
     l.close()
