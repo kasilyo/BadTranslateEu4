43bff271bac951b04d164ebd02a6c699b2f9a043
Kasilyo <kasiyokiwi@gmail.com>
2024-05-29 16:38:18 -0600

    Added francify

---
+++ francify.py
@@ -0,0 +1,26 @@
+import os
+import yaml
+
+
+z = os.listdir("localization")
+# print(z)
+for fp in z:
+    l = open("localization\\" + fp, "r", encoding="UTF-8")
+    lines = l.readlines()
+    sp = None
+    nl = []
+    temp = ""
+    nl.append(lines[0])
+    for x in lines[1:]:
+        if x.find("#") == -1:
+            sp = x.split(" \"")
+            tmp = sp[0] + " \"France\"\n"
+            nl.append(tmp)
+    l.close()
+    l = open("localization\\" + fp, "w", encoding="UTF-8")
+    for x in nl:
+        l.write(x)
+    l.close()
+
+
+print(nl)
